321ed26a80e79adadaccfa3fe997eb0b378f52d0
sohaibbinmohsin <62615453+sohaibbinmohsin@users.noreply.github.com>
2022-12-20 09:37:13 +0500

    Update

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    // Prompt the user to enter a number
+    println!("Enter a number:");
+
+    // Read the number from the user
+    let mut num = String::new();
+    std::io::stdin().read_line(&mut num)
+        .expect("Failed to read line");
+
+    // Parse the number into a u32 value
+    let num: u32 = match num.trim().parse() {
+        Ok(n) => n,
+        Err(_) => {
+            println!("Invalid number");
+            return;
+        },
+    };
+
+    // Generate the multiplication table for the given number
+    for i in 1..=10 {
+        println!("{} x {} = {}", num, i, num * i);
+    }
+}
